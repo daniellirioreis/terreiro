a006166bac7eba20f2ce7e88ee0c3c1f7d691630
Daniel Lirio Reis <daniellirioreis@gmail.com>
2016-07-05 18:36:42 -0300

    Teste de Noticias na pagina de login do sistema

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :authenticate_user!
-  
-  
-  
+
+  layout :define_layout
   
   private
 	
@@ -18,5 +17,14 @@ class ApplicationController < ActionController::Base
   def define_environment_message
     message[:alert] ||= []
   end
+  
+  def define_layout
+    if user_signed_in?
+    else
+      'login'
+    end 
+
+  end
+  
 	
 end
