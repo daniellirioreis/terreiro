4073e9b65d3946c25843fb964de5235d34455c4a
Daniel Lirio Reis <daniellirioreis@gmail.com>
2016-07-22 12:27:59 -0300

    Estruturação da pagina, e menu de estudos

--- app/models/study.rb
+++ app/models/study.rb
@@ -1,4 +1,8 @@
 class Study < ActiveRecord::Base
   has_many :study_pages, dependent: :destroy
   accepts_nested_attributes_for :study_pages, reject_if: :all_blank, allow_destroy: true
+
+  def to_s
+    name
+  end
 end
