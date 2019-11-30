0b8618dfb770b4d1a3ffe7f794807dcbb254d2f1
Daniel Lirio Reis <daniellirioreis@gmail.com>
2019-11-30 19:20:00 -0300

    correcao

--- app/models/event_patient.rb
+++ app/models/event_patient.rb
@@ -4,7 +4,7 @@ class EventPatient < ActiveRecord::Base
   has_enumeration_for :status_event_patient, with: StatusEventPatient, create_helpers: true
 
   scope :by_status_event_patient, lambda { |status| where("event_patients.status_event_patient = ?", status)}
-  scope :sorted, -> { order(:name).joins(:patient) }
+  scope :sorted, -> { order("patients.name").joins(:patient) }
   
 
   def to_s
