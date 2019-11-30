8933f6bf46e4852f5db32cdbf6e94cd6731a3b9c
Daniel Lirio Reis <daniellirioreis@gmail.com>
2019-11-30 19:13:34 -0300

    ordernar

--- app/models/event_patient.rb
+++ app/models/event_patient.rb
@@ -4,6 +4,8 @@ class EventPatient < ActiveRecord::Base
   has_enumeration_for :status_event_patient, with: StatusEventPatient, create_helpers: true
 
   scope :by_status_event_patient, lambda { |status| where("event_patients.status_event_patient = ?", status)}
+  scope :sorted, -> { order(:name).joins(:patient) }
+  
 
   def to_s
     patient
